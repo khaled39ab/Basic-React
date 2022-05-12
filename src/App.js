3538b7795b101e5277fcc2b5ebd4abe6d257d44b
khaled39ab <khaled39ab@gmail.com>
2022-05-12 22:36:28 +0600

    External css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Earn React
-        </a>
-      </header>
+      <h1>Basic React</h1>
+      <ExternalCss></ExternalCss>
     </div>
   );
+}
+
+function ExternalCss() {
+  return (
+    <div className='greeting'>
+      <h3>External Css</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, ab debitis! Sequi alias, maxime dignissimos facilis tenetur dolore placeat ipsa aliquam neque fuga? Inventore, sunt error. Odio aliquid voluptate labore.</p>
+    </div>
+  )
 }
 
 export default App;
